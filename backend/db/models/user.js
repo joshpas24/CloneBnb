75ed26fe5d7531592705be689a844b38b7f00e8d
Josh Pascual <joshapascual@gmail.com>
2023-06-09 10:36:14 -0700

    name null 2

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -16,9 +16,11 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     firstName: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     lastName: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     username: {
       type: DataTypes.STRING,
