48dea0a59df8354a8cd2571d1845f0ccfcdd2fca
Josh Pascual <joshapascual@gmail.com>
2023-06-13 08:36:14 -0700

    options added to migrations

--- backend/db/migrations/20230612205542-create-booking.js
+++ backend/db/migrations/20230612205542-create-booking.js
@@ -1,5 +1,11 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Bookings', {
@@ -41,7 +47,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
 
     await queryInterface.addConstraint('Bookings', {
       fields: ['endDate'],
@@ -50,10 +56,11 @@ module.exports = {
         endDate: { [Sequelize.Op.gt]: Sequelize.col('startDate') },
       },
       name: 'Bookings_endDate_check'
-    });
+    }, options);
 
   },
   async down(queryInterface, Sequelize) {
+    options.tableName = 'Bookings'
     await queryInterface.removeConstraint('Bookings', 'Bookings_endDate_check');
     await queryInterface.dropTable('Bookings');
   }
