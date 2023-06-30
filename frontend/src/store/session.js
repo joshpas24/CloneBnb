3f586e1a63b1262a27ed11dc6afbc40815d8d522
Josh Pascual <joshapascual@gmail.com>
2023-06-30 15:41:11 -0700

    nav completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,11 +1,11 @@
 import { csrfFetch } from "./csrf";
 
 //ACTION TYPE CONSTAINTS
-export const ADD_USER = "session/addUser";
+export const ADD_USER = "session/setUser";
 export const REMOVE_USER = "session/removeUser";
 
 //ACTION CREATORS
-export const addUser = (user) => {
+export const setUser = (user) => {
     return {
         type: ADD_USER,
         user
@@ -30,10 +30,44 @@ const { credential, password } = user;
   });
   const data = await response.json();
   console.log("data from thunk: ", data)
-  dispatch(addUser(data.user));
+  dispatch(setUser(data.user));
   return response;
 }
 
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+
+//REDUCER
 const initialState = { user: null }
 
 const sessionReducer = (state = initialState, action) => {
